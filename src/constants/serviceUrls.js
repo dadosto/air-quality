a0e65b91899abf6282926e85c3a47ecf3395e026
Dado <dadosto@gmail.com>
2017-04-12 00:37:21 +0200

    Added service urls file

---
+++ src/constants/serviceUrls.js
@@ -0,0 +1,11 @@
+const serviceBaseUrl = 'https://api.breezometer.com/baqi/';
+const queryParamStartDateTime = 'start_datetime';
+const queryParamEndDateTime = 'end_datetime';
+const queryParamInterval = 'interval';
+const queryParamLatitude = 'lat';
+const queryParamLongitude = 'lon';
+const queryParamKey = 'key';
+
+const apiKey = '005b69208ac0466d9dda0ac071349717';
+
+export const getDataFromServerUrl = `${serviceBaseUrl}?`;
