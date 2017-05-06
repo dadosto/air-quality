4b688051632d8d7243e7e08fc177b2b881c7466e
Slavcho Arsovski <slavcho.arsovski@netcetera.com>
2017-05-06 20:58:39 +0200

    changed initial state

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -5,30 +5,30 @@ import {
 
 export const initialState = {
   data: {
-    datetime: "2017-01-01T01:00:00",
-    country_name: "United States",
-    breezometer_aqi: 82,
-    breezometer_color: "#4FBB35",
-    breezometer_description: "Excellent air quality",
-    country_aqi: 22,
-    country_aqi_prefix: "",
-    country_color: "#00E400",
-    country_description: "Good air quality",
-    data_valid: true,
-    key_valid: true,
-    random_recommendations: {
-      children: "The air is ideal for outdoor activities with infants and children",
-      sport: "This is an excellent opportunity to enjoy your favorite outdoor activity without any worries",
-      health: "With this particular air quality, you have no limitations. Enjoy the environment!",
-      inside: "Open the windows, let that fresh air come in & go outside!",
-      outside: "It's an ideal time to go outside and enjoy the fresh air"
+    "datetime":"2017-05-06T17:55:15",
+    "country_name":"Macedonia",
+    "breezometer_aqi":66,
+    "breezometer_color":"#9CD828",
+    "breezometer_description":"Fair air quality",
+    "country_aqi":null,
+    "country_aqi_prefix":"",
+    "country_color":"#99CC00",
+    "country_description":"Low air pollution",
+    "data_valid":true,
+    "key_valid":true,
+    "random_recommendations":{
+      "children":"No reason to panic, but pay attention to changes in air quality and any signals of breathing problems in your children",
+      "sport":"Since we inhale more air during sports, you should keep track of changes in air quality for the next few hours",
+      "health":"Exposure to air hazards is dangerous for people with health sensitivities, so it is important to monitor air quality at this time",
+      "inside":"The amount of pollutants in the air is noticeable, but still there is no danger to your health - It is recommended to continue monitoring changes in the coming hours",
+      "outside":"It's still OK to go out and enjoy a stroll, just pay attention for changes in air quality"
     },
-    dominant_pollutant_canonical_name: "pm25",
-    dominant_pollutant_description: "Fine particulate matter (<2.5µm)",
-    dominant_pollutant_text: {
-      main: "The dominant pollutant is fine particulate matter (PM2.5).",
-      effects: "Particles enter the lungs and cause local and systemic inflammation in the respiratory system & heart, thus cause cardiovascular and respiratory diseases such as asthma and bronchitis.",
-      causes: "Main sources are fuel burning processes in industry, transportation and indoor heating."
+    "dominant_pollutant_canonical_name":"o3",
+    "dominant_pollutant_description":"Ozone",
+    "dominant_pollutant_text":{
+      "main":"The dominant pollutant is ozone (O₃).",
+      "effects":"Ozone can irritate the airways causing coughing, a burning sensation, wheezing and shortness of breath. Children, people with respiratory or lung and heart diseases, elderly and those who exercise outdoors are particularly vulnerable to ozone exposure.",
+      "causes":"Ozone is created in a chemical reaction between atmospheric oxygen, nitrogen oxides, organic compounds and sunlight."
     }
   },
   location: {
